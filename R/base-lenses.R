e31d8741c09433300aa71c3394d7116b426dc9e7
Albert Z Wang <3493079+arbelt@users.noreply.github.com>
2024-11-14 12:37:15 -0500

    fix: remove unnecessary tibble dep

--- R/base-lenses.R
+++ R/base-lenses.R
@@ -112,17 +112,15 @@ vec_data_l <- lens(
 #' @return A lens that selects the specified elements
 #'
 #' @export
-map_l <- function(l, .ptype = list()) {
+map_l <- function(l, .ptype = NULL) {
   .view <- function(d) {
-    new_d <- lapply(d, \(x) view(x, l))
+    new_d <- d
+    new_d[] <- lapply(d, \(x) view(x, l))
     if (!is.list(d) && all(vapply(new_d, rlang::is_scalar_atomic, logical(1)))) {
       return(unlist(new_d, recursive = FALSE))
     }
     if (rlang::is_atomic(.ptype) && all(vapply(new_d, rlang::is_scalar_atomic, logical(1)))) {
       return(unlist(new_d, recursive = FALSE))
-    }
-    if (vec_is(.ptype, data.frame())) {
-      return(tibble::as_tibble(new_d))
     }
     new_d
   }
@@ -130,15 +128,13 @@ map_l <- function(l, .ptype = list()) {
     if (is.null(x)) {
       x <- list(NULL)
     }
-    new_d <- mapply(l@set, d, x, SIMPLIFY = FALSE)
+    new_d <- d
+    new_d[] <- mapply(l@set, d, x, SIMPLIFY = FALSE)
     if (!is.list(d) && all(vapply(new_d, rlang::is_scalar_atomic))) {
       return(unlist(new_d, recursive = FALSE))
     }
     if (rlang::is_atomic(.ptype) && all(vapply(new_d, rlang::is_scalar_atomic))) {
       return(unlist(new_d, recursive = FALSE))
-    }
-    if (vec_is(.ptype, data.frame())) {
-      return(tibble::as_tibble(new_d))
     }
     new_d
   }
