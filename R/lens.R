321d7ac2677531e91c51a60050c0e50e0a63dd6a
Albert Wang <3493079+arbelt@users.noreply.github.com>
2024-12-02 13:55:15 -0500

    docs: fix constructor sig take 2

--- R/lens.R
+++ R/lens.R
@@ -8,8 +8,8 @@
 #' - **View-Set**: `set(d, l, view(d, l)) == d`
 #' - **Set-View**: `view(set(d, l, x), l) == x`
 #' - **Set-Set**: `set(set(d, l, x), l, y) == set(d, l, y)`
-#' 
-#' These laws are not enforced by `tinylens`, but you should strive to follow them  
+#'
+#' These laws are not enforced by `tinylens`, but you should strive to follow them
 #' when creating your own lenses.
 #'
 #' A best effort has been made to ensure that these laws hold for the lenses
@@ -24,8 +24,16 @@
 #' # create a trivial identity lens
 #' l <- lens(view = function(x) x, set = function(x, value) value)
 lens <- S7::new_class("lens",
-                      package = packageName(),
-                      properties = list(
-                        "view" = NULL | class_function,
-                        "set" = NULL | class_function
-                      ))
+  package = packageName(),
+  properties = list(
+    "view" = NULL | class_function,
+    "set" = NULL | class_function
+  ),
+  constructor = function(view, set = NULL) {
+    S7::new_object(
+      S7::S7_object(),
+      view = view,
+      set = set
+    )
+  }
+)
